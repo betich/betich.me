80e6097588af2a6e320a6fe944bfae93375169b9
betich <betich365@gmail.com>
2022-03-08 21:57:59 +0700

    favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,11 +21,10 @@ export default class Document extends NextDocument {
             href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:wght@400;500;600;700;800&display=swap"
             rel="stylesheet"
           />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/meta/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/meta/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/meta/favicon-16x16.png" />
+          <link rel="manifest" href="/meta/site.webmanifest" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-TX1HHP7FPJ"></script>

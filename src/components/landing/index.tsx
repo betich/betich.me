6d1f7e7be16988e135b533f263fed4cb601dc0d0
betich <betich365@gmail.com>
2024-04-17 02:14:23 +0700

    feat: add content

--- src/components/landing/index.tsx
+++ src/components/landing/index.tsx
@@ -1,22 +1,24 @@
 import useMousePosition from "@/utils/useMousePosition";
 
 export default function Landing() {
-  const { x, y } = useMousePosition();
+  // const { x, y } = useMousePosition();
 
   return (
-    <main className="bg-primary cursor-none relative text-white p-20 min-h-screen w-full flex flex-col gap-4">
-      <h1 className="font-bold text-4xl">hi</h1>
+    <>
+      <h1 className="text-lg font-bold">About</h1>
+
+      <p>Hi, my name is Thee. I'm a full-stack developer based in Bangkok, Thailand.</p>
 
       {/* spawn a circle around the mouse */}
-      {x && y && (
+      {/* {x && y && (
         <div
-          className="absolute w-4 h-4 bg-white rounded-full"
+          className="absolute h-4 w-4 rounded-full bg-white"
           style={{
             top: y - 2,
             left: x - 2,
           }}
         />
-      )}
-    </main>
+      )} */}
+    </>
   );
 }

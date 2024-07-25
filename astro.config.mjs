6a882c72ee0869ed22c9e675529698fe3338d27d
betich <betich365@gmail.com>
2024-07-25 19:36:53 +0700

    feat: very cool version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react({
-      include: ["./src/**/*.tsx", "./src/**/*.jsx", "./src/**/*.js"],
-    }),
-    tailwind(),
-  ],
+  integrations: [tailwind(), react()]
 });

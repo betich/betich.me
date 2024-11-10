accdabaf74607779bcc23490c6e998431cefc073
betich <betich365@gmail.com>
2024-11-11 04:59:12 +0700

    fix(ssr): upgrade astro version to fully implement ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import partytown from "@astrojs/partytown";
-import rss from "@astrojs/rss";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
   site: "https://betich.me",
   integrations: [
+    // sitemap(),
     tailwind(),
     react(),
-    rss(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
   ],
 });
